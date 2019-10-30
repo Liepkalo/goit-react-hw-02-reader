187776687d415c04a1fe42b217739a24b5a98a71
Oleg Liepkalo <liepkalo.o@gmail.com>
2019-10-30 23:21:39 +0200

    Updates

---
+++ precache-manifest.7176e3f9fbd7ea4c120c4cf9b93ddb6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9822892eaa9a418acba65ee7af8f00f",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "59ca006b10cb778231f0",
+    "url": "/goit-react-hw-02-reader/static/css/main.074bb251.chunk.css"
+  },
+  {
+    "revision": "f71114313b3e8375b744",
+    "url": "/goit-react-hw-02-reader/static/js/2.12ee2cfe.chunk.js"
+  },
+  {
+    "revision": "59ca006b10cb778231f0",
+    "url": "/goit-react-hw-02-reader/static/js/main.648eec0e.chunk.js"
+  },
+  {
+    "revision": "46b5d60be30d4a7b06da",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.7bf316d8.js"
+  }
+]);
